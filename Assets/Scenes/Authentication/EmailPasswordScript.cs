b3f2a48e5e2788afa868850bbe3ac7fecae63659
Takumi Sato <sato.uso.0401@gmail.com>
2021-06-18 19:43:28 +0900

    サインアップとログインの分離

--- Assets/Scenes/Authentication/EmailPasswordScript.cs
+++ Assets/Scenes/Authentication/EmailPasswordScript.cs
@@ -10,36 +10,36 @@ namespace Scenes.Authentication
     {
         private string _apiKey = Config.ApiKey;
 
-        public void Submit()
+        public void SignUp()
         {
             var inputEmail = GameObject.Find("InputEmail").GetComponent<InputField>().text;
             var inputPassword = GameObject.Find("InputPassword").GetComponent<InputField>().text;
             var resultText = GameObject.Find("ResultText").GetComponent<Text>();
 
-            SignUp(inputEmail, inputPassword);
-            Login(inputEmail, inputPassword);
-
-            resultText.text = "サインアップとログインに成功しました";
-        }
-
-        void SignUp(string email, string password)
-        {
             WebClient wc = new WebClient();
             wc.Headers[HttpRequestHeader.ContentType] = "application/json";
             var url = $"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={_apiKey}";
-            var requestBody = new FirebaseApi.EmailPasswordAuthRequest(email, password).ToJson();
+            var requestBody = new FirebaseApi.EmailPasswordAuthRequest(inputEmail, inputPassword).ToJson();
             string response = wc.UploadString(new Uri(url), requestBody);
             Debug.Log(response);
+
+            resultText.text = "サインアップに成功しました";
         }
 
-        void Login(string email, string password)
+        public void Login()
         {
+            var inputEmail = GameObject.Find("InputEmail").GetComponent<InputField>().text;
+            var inputPassword = GameObject.Find("InputPassword").GetComponent<InputField>().text;
+            var resultText = GameObject.Find("ResultText").GetComponent<Text>();
+
             WebClient wc = new WebClient();
             wc.Headers[HttpRequestHeader.ContentType] = "application/json";
             var url = $"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={_apiKey}";
-            var requestBody = new FirebaseApi.EmailPasswordAuthRequest(email, password).ToJson();
+            var requestBody = new FirebaseApi.EmailPasswordAuthRequest(inputEmail, inputPassword).ToJson();
             string response = wc.UploadString(new Uri(url), requestBody);
             Debug.Log(response);
+
+            resultText.text = "ログインに成功しました";
         }
     }
 }
