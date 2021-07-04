544d41ac3649733cadbc93cd225a90d53b72e065
Takumi Sato <sato.uso.0401@gmail.com>
2021-07-04 17:09:01 +0900

    リフレッシュトークンの更新

--- Assets/Scenes/Authentication/RefreshIdTokenScript.cs
+++ Assets/Scenes/Authentication/RefreshIdTokenScript.cs
@@ -14,7 +14,7 @@ namespace Scenes.Authentication
 
         public void GetRefreshToken()
         {
-            var email = "assafnativ@example.com";
+            var email = "assafnativ2@example.com";
             var password = "dolphins";
             WebClient wc = new WebClient();
             wc.Headers[HttpRequestHeader.ContentType] = "application/json";
@@ -39,6 +39,11 @@ namespace Scenes.Authentication
             nvs.Add("refresh_token", _refreshToken);
             var response = wc.UploadValues(new Uri(url), nvs);
             var idTokenResult = FirebaseApi.RefreshIdTokenResponse.FromJson(Encoding.UTF8.GetString(response));
+            // ドキュメントを読む限りRefresh Tokenは時間経過では無効にはならないので、
+            // 一度サインアップ時に受け取ったRefresh Tokenは使いまわせるはずだが(実際古いRefresh Tokenでも問題なく使える)、
+            // idTokenの更新した際のレスポンスのrefresh_tokenは毎回変わるので念のため新しいものに置き換える
+            // https://firebase.google.com/docs/auth/admin/manage-sessions?hl=ja
+            _refreshToken = idTokenResult.refresh_token;
 
             Debug.Log($"RefreshIdToken idToken: {idTokenResult.id_token}");
             Debug.Log($"RefreshIdToken refreshToken: {idTokenResult.refresh_token}");
